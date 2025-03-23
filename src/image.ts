1568967caaf6a4b55060ea3a880711992d2a3568
kouxi08 <kouxi14@gmail.com>
2025-03-23 22:40:24 +0900

    update image.ts

--- src/image.ts
+++ src/image.ts
@@ -1,5 +1,5 @@
 import url from 'node:url'
-import http from 'node:http'
+import https from 'https'
 import { imageSize } from 'image-size'
 import type { Photo } from "react-photo-album";
 
@@ -18,7 +18,7 @@ export async function get_photo(image: string):Promise<Photo> {
                 height: Math.round((results.height / results.width) * breakpoint),
             }))
         }
-        
+
         return images
     }catch(error){
         throw error;
@@ -27,38 +27,39 @@ export async function get_photo(image: string):Promise<Photo> {
 
 export async function get_image_size(imgUrl: string): Promise<{ width: number; height: number }> {
     return new Promise((resolve, reject) => {
-        const options = url.parse(imgUrl);
+      const parsedUrl = url.parse(imgUrl);
+      const client = parsedUrl.protocol === 'https:' ? https : https;
 
-        http.get(options, (response) => {
+      client.get(parsedUrl, (response) => {
         const chunks: any[] = [];
 
-            response
-                .on('data', (chunk) => {
-                chunks.push(chunk);
-                })
-                .on('end', () => {
-                try {
-                    const buffer = Buffer.concat(chunks);
-                    const dimensions = imageSize(buffer);
-                    // console.log(dimensions.height, dimensions.width)
+        response
+          .on('data', (chunk) => {
+            chunks.push(chunk);
+          })
+          .on('end', () => {
+            try {
+              const buffer = Buffer.concat(chunks);
+              const dimensions = imageSize(buffer);
 
-                    if (!dimensions.width || !dimensions.height) {
-                    reject(new Error('Could not determine image size.'));
-                    return;
-                    }
-                    resolve({
-                    width: dimensions.width,
-                    height: dimensions.height,
-                    });
-                } catch (err) {
-                    reject(err);
-                }
-                })
-                .on('error', (err) => {
-                    reject(err);
-                });
-        }).on('error', (err) => {
+              if (!dimensions.width || !dimensions.height) {
+                reject(new Error('Could not determine image size.'));
+                return;
+              }
+
+              resolve({
+                width: dimensions.width,
+                height: dimensions.height,
+              });
+            } catch (err) {
+              reject(err);
+            }
+          })
+          .on('error', (err) => {
             reject(err);
-        });
+          });
+      }).on('error', (err) => {
+        reject(err);
+      });
     });
-}
+  }
